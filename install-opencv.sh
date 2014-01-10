bc6e25185c903139f2efcd5dd4c5bb7e6760caed
Anthony Spears <aspears@gatech.edu>
2014-01-10 13:25:44 -0500

    first commit

---
+++ install-opencv.sh
@@ -0,0 +1,30 @@
+
+cd ~
+
+#install OPENCV Dependency Packages
+sudo apt-get install build-essential
+sudo apt-get install cmake
+sudo apt-get install git
+sudo apt-get install libgtk2.0-dev
+sudo apt-get install pkg-config
+sudo apt-get install python-dev
+sudo apt-get install python-numpy
+sudo apt-get install libavcodec-dev
+sudo apt-get install libavformat-dev 
+sudo apt-get install libswscale-dev
+
+sudo apt-get install libdc1394-22 	#Change this version if needed
+sudo apt-get install libjpeg-dev
+sudo apt-get install libpng-dev
+sudo apt-get install libtiff-dev
+sudo apt-get install libjasper-dev
+
+#install OPENCV
+mkdir opencv
+cd opencv
+mkdir bin
+mkdir src
+
+#download the opencv pkg from srcforge and extract it to the src dir. Then make it into the bin dir.
+
+
